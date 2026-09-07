a405b9e4c45622564c99e1b76f3f8512a67eb7c3
AdrienPecher <adri.pcr0301@gmail.com>
2026-09-07 15:49:50 +0200

    fix(db): renommer DB_PASS en DB_PASSWORD pour Railway

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASS,
+  process.env.DB_PASSWORD,
   {
     host:    process.env.DB_HOST || 'localhost',
     port:    process.env.DB_PORT || 3306,
